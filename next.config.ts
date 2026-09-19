17c24c9e04e0c07af58f850d26a0f889deae5746
Adarshtz <adarshtz313@gmail.com>
2026-09-19 20:12:16 +0530

    deployment fix.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/mvp-calculator",
-  assetPrefix: "/mvp-calculator",
   images: {
     unoptimized: true,
   },
